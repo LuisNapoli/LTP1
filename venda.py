bb52884d488690d4043d1a63e7e0c1fc4404c31f
luis.napoli <luis.napoli@sempreceub.com>
2018-11-29 19:32:27 -0200

    trab

--- venda.py
+++ venda.py
@@ -4,8 +4,3 @@ class Venda:
         self.vendedor = venda
         self.comprador = compra
         self.preco = preco
-
-    def lucro(self):
-        return self.preco - self.carro.get_preco()
-    def info_venda(self):
-        return self.carro.get_dados(), self.vendedor.get_dados(), self.comprador.get_dados(), self.preco, self.lucro()
